c866509f4b59575fb43bee2c0b9116ec34eec2fa
KaidenTBIO <coutogabriel3@gmail.com>
2018-08-27 09:45:38 -0300

    Add NN functionalities; code available for refactoring; find better training data storage method; by default, only numbers 1-4 supported out of planned 10 (0-9)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+const config = {
+	binaryThresh: 0.5,
+	inputSize: 25,
+	hiddenLayer: [10, 10, 10, 5],
+	activation: 'sigmoid'
+}
+
 let input = [0, 0, 0, 0, 0,
 			 0, 0, 0, 0, 0,
 			 0, 0, 0, 0, 0,
@@ -5,6 +12,12 @@ let input = [0, 0, 0, 0, 0,
 			 0, 0, 0, 0, 0];
 
 $(document).ready(function(){
+	const net = new brain.NeuralNetwork(config);
+
+	net.train([train1.v1, train1.v2, train1.v3,
+			  train2.v1, train2.v2, train2.v3,
+			  train3.v1, train3.v2, train3.v3]);
+
 	$(".pixel").click(function(event) {
 		var idx = parseInt($(this).attr('id'));
 		
@@ -18,5 +31,35 @@ $(document).ready(function(){
 
 		$(this).toggleClass('pixel darkPixel');
 		console.log(input);
-	})
+	});
+
+	$(".getOutput").click(function(event) {
+		const output = net.run(input);
+
+		var max = output[0];
+		var maxIndex = 0;
+
+		for (let i = 0; i < output.length; i++) {
+			if (output[i] > max) {
+				maxIndex = i;
+				max = output[i];
+			}
+		}
+		console.log(input);
+		//document.getElementById("result").value = maxIndex;
+		$("#result").text(maxIndex.toString());
+	});
+
+	$(".inputforTraining").click(function(event) {
+		var output = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+		var idx = parseInt(document.getElementById("trainOutput").value);
+		output[idx] = 1;
+
+		var trainNow = {
+			input: input,
+			output: output
+		};
+
+		net.train(trainNow);
+	});
 });
